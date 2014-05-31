c3878f6932d0c531b2f38767ed81cf1f624bd414
Abdelhak Bougouffa <abdelhak@cryptolab.net>
2014-05-31 16:36:14 +0100

    Add 'shift' optional parametre to the prayer times methods

--- pyIslam/praytimes.py
+++ pyIslam/praytimes.py
@@ -87,50 +87,14 @@ class Prayer: # Prayer times and qiblah calculating class
         return (180 / (4 * atan(1))) * atan(x / sqrt(-x * x + 1))
 
 
-    def fajrTime(self): # Fajr Time
-        '''Get the Fajr time'''
-        return Prayer._Prayer__hoursToTime(self.__dohrTime() - self.__prayerTime(self.__conf.fajrZenith))
-
-
-    def sherookTime(self): # Sherook Time
-        '''Get the Sunrise (Sherook) time'''
-        return Prayer._Prayer__hoursToTime(self.__dohrTime() - self.__prayerTime(self.__conf.sherookZenith))
-
-
-    def __dohrTime(self): # Dohr time for internal use, return number of hours, not time object
+    def __dohrTime(self, shift): # Dohr time for internal use, return number of hours, not time object
         '''Get the Dohr time'''
+        if not (isinstance(shift, float) or isinstance(shift, int)):
+            raise Exception("'shift' value must be an 'int' or 'float'")
         ld = self.__conf.longitudeDifference
         time_eq = self.__equationOfTime()
         duhr_t = 12 + ld + time_eq / 60
-        return duhr_t
-
-
-    def dohrTime(self): # Dohr Time
-        return Prayer._Prayer__hoursToTime(self.__dohrTime())
-
-
-    def asrTime(self): # Asr Time
-        '''Get the Asr time'''
-        return Prayer._Prayer__hoursToTime(self.__dohrTime() + self.__prayerTime(self.__asrZenith()))
-
-
-    def maghrebTime(self): # Maghreb Time
-        '''Get the Maghreb time'''
-        return Prayer._Prayer__hoursToTime(self.__dohrTime() + self.__prayerTime(self.__conf.maghrebZenith))
-
-
-    def ishaaTime(self): # ishaa Time
-        '''Get the Ishaa time'''
-        if (self.__conf.ishaaZenith == 0.0):
-            hij = today(1)
-            if (hij.month == 9):
-                ishaa_t = self.maghreb_time() + 2.0 # 2.0 hours = 120 minutes
-            else:
-                ishaa_t = self.maghreb_time() + 1.5 # 1.5 hours = 90 minutes
-        else:
-            ishaa_t = self.__prayerTime(self.__conf.ishaaZenith)
-            ishaa_t = self.__dohrTime() + ishaa_t
-        return Prayer._Prayer__hoursToTime(ishaa_t)
+        return shift/3600 + duhr_t
 
 
     def __prayerTime(self, zenith): # Get Times for "Fajr, Sherook, Asr, Maghreb, ishaa"
@@ -146,3 +110,41 @@ class Prayer: # Prayer times and qiblah calculating class
         minutes = (hours - int(hours)) * 60
         seconds = (minutes - int(minutes)) * 60
         return time((int(hours) + st), int(minutes), int(seconds))
+
+
+    def fajrTime(self, shift=0.0): # Fajr Time
+        '''Get the Fajr time'''
+        return Prayer._Prayer__hoursToTime(self.__dohrTime(shift) - self.__prayerTime(self.__conf.fajrZenith) )
+
+
+    def sherookTime(self, shift=0.0): # Sherook Time
+        '''Get the Sunrise (Sherook) time'''
+        return Prayer._Prayer__hoursToTime(self.__dohrTime(shift) - self.__prayerTime(self.__conf.sherookZenith))
+
+
+    def dohrTime(self, shift=0.0): # Dohr Time
+        return Prayer._Prayer__hoursToTime(self.__dohrTime(shift))
+
+
+    def asrTime(self, shift=0.0): # Asr Time
+        '''Get the Asr time'''
+        return Prayer._Prayer__hoursToTime(self.__dohrTime(shift) + self.__prayerTime(self.__asrZenith()))
+
+
+    def maghrebTime(self, shift=0.0): # Maghreb Time
+        '''Get the Maghreb time'''
+        return Prayer._Prayer__hoursToTime(self.__dohrTime(shift) + self.__prayerTime(self.__conf.maghrebZenith))
+
+
+    def ishaaTime(self, shift=0.0): # ishaa Time
+        '''Get the Ishaa time'''
+        if (self.__conf.ishaaZenith == 0.0):
+            hij = today(1)
+            if (hij.month == 9):
+                ishaa_t = self.maghreb_time(shift) + 2.0 # 2.0 hours = 120 minutes
+            else:
+                ishaa_t = self.maghreb_time(shift) + 1.5 # 1.5 hours = 90 minutes
+        else:
+            ishaa_t = self.__prayerTime(self.__conf.ishaaZenith)
+            ishaa_t = self.__dohrTime(shift) + ishaa_t
+        return Prayer._Prayer__hoursToTime(ishaa_t)
