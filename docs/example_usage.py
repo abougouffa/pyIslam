1399824d44efa584d43065af3766502a0487c0f3
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2020-06-08 16:57:07 +0200

    Update docs/example_usage.py

--- docs/example_usage.py
+++ docs/example_usage.py
@@ -4,10 +4,11 @@
 from pyIslam.praytimes import PrayerConf, Prayer, LIST_FAJR_ISHA_METHODS
 from pyIslam.hijri import HijriDate
 from pyIslam.qiblah import Qiblah
-from datetime import date
+from datetime import date, datetime
 from pyIslam.mirath import Mirath
 from pyIslam.zakat import Zakat
-
+from pyIslam.baselib import gregorian_to_julian
+import pyIslam.baselib_unit_test
 
 # Latitude = 36.716667
 # Longitude = 3.250000
@@ -20,11 +21,11 @@ ar = ('Shafii, Maliki, Hambali', 'Hanafi')
 longitude = input('1. Enter the longitude of your city: ')
 
 if longitude == '':
-    print('\nUsing the default values: Country = Algeria, State = Algiers')
-    longitude = 3.250000
-    latitude = 36.716667
-    timezone = 1
-    fajr_isha_method = 3
+    print('\nUsing the default values: Country = France, State = Algiers')
+    longitude = 0.1123912
+    latitude = 45.6186566
+    timezone = 2
+    fajr_isha_method = 6
     asr_fiqh = 1
 else:
     longitude = float(longitude)
@@ -64,12 +65,15 @@ print('Fajr and Ishaa reference:\n\t',
 print('Asr madhab:\n\t', ar[asr_fiqh - 1])
 print('\nPrayer times for: ' + hijri.format(2) + ' '
       + str(hijri.to_gregorian()))
-print('Fajr:    ' + str(pt.fajr_time()))
-print('Sherook: ' + str(pt.sherook_time()))
-print('Dohr:    ' + str(pt.dohr_time()))
-print('Asr:     ' + str(pt.asr_time()))
-print('Maghreb: ' + str(pt.maghreb_time()))
-print('Ishaa:   ' + str(pt.ishaa_time()))
+print('Fajr      : ' + str(pt.fajr_time()))
+print('Sherook   : ' + str(pt.sherook_time()))
+print('Dohr      : ' + str(pt.dohr_time()))
+print('Asr       : ' + str(pt.asr_time()))
+print('Maghreb   : ' + str(pt.maghreb_time()))
+print('Ishaa     : ' + str(pt.ishaa_time()))
+print('1st third : ' + str(pt.second_third_of_night()))
+print('Midnight  : ' + str(pt.midnight()))
+print('Qiyam     : ' + str(pt.last_third_of_night()))
 
 print('Qiblah direction from the north: ' + Qiblah(pconf).sixty())
 
@@ -80,7 +84,15 @@ print(str(z.calculate_zakat(10000)) + ' $')
 print(str(z.calculate_zakat_harvest(10000)) + ' Kg')
 print(str(z.calculate_zakat_harvest(10000, 'natural', 'other')) + ' Kg')
 
-print('\n---testing mirath---\n')
+
+print("Julian Day (from datetime.now): {}".format(
+    gregorian_to_julian(datetime.now())))
+
+print("Julian Day (from date.today): {}".format(
+    gregorian_to_julian(date.today())))
+
+
+print('\n---Testing Mirath---\n')
 
 test = Mirath()
 test.add_relative('wife')
